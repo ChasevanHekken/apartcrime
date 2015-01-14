9159f97e73e8a060a92784cca404aff36fafd453
ChasevanHekken <cvh1717@gmail.com>
2015-01-14 16:25:27 -0700

    data points on map

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'auth/twitter/callback', to: 'sessions#create'
   get '/login' => redirect('/auth/twitter'), as: :login
+
   resource :sessions, :only => [:create]
 
   get 'home/index'

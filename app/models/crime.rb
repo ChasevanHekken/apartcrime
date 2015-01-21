2b86b13a15e0e6b5e0cd7cda25830f93fc3b4412
ChasevanHekken <cvh1717@gmail.com>
2015-01-21 13:25:14 -0700

    no null markers

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,7 +1,7 @@
 class Crime < ActiveRecord::Base
 
   def self.get_coordinates
-    Crime.limit(10000).pluck(:point_y, :point_x).sample(3000)
+    Crime.limit(10000).pluck(:point_y, :point_x).sample(300)
   end
 
 end

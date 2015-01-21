16e336d015b8993d2af7988c341749d642657e51
ChasevanHekken <cvh1717@gmail.com>
2015-01-21 08:23:43 -0700

    null filter

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,7 +1,7 @@
 class Crime < ActiveRecord::Base
 
   def self.get_coordinates
-    Crime.limit(100000).pluck(:point_y, :point_x).sample(10000)
+    Crime.limit(10000).pluck(:point_y, :point_x).sample(3000)
   end
 
 end

018945b1e380dea36e6f6433d63330629b6317ab
ChasevanHekken <cvh1717@gmail.com>
2015-01-22 13:31:38 -0700

    not sure

--- app/services/apart_service.rb
+++ app/services/apart_service.rb
@@ -3,23 +3,13 @@ class ApartService
 
   def initialize
     @connection = Faraday.new(url: "http://search.3taps.com/?auth_token=8cc7402e899fbc9d66f3b797758aaa9b&rpp=100&source=CRAIG&location.metro=USA-PHI&category=RHFR&location.county=USA-PA-PHI&location.city=USA-PHI-PHI")
+    apart_coords
   end
 
   def apart_coords
-    parse(connection.get)
+    @marker_data = parse(connection.get)
+
   end
-
-  # def schools
-  #   parse(connection.get("schools"))
-  # end
-
-  # def school(id)
-  #   parse(connection.get("schools/#{id}"))
-  # end
-
-  # def create_school(params)
-  #   parse(connection.post("schools", params))
-  # end
 
   private
 

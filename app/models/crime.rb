f4d6789a13aa099cc945fa1f8ec92eb53a8ed065
ChasevanHekken <cvh1717@gmail.com>
2015-01-29 11:40:46 -0700

    kinda got shit working

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,30 +1,30 @@
 class Crime < ActiveRecord::Base
   def self.homicide
-    Crime.where(ucr_general: 100).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 100).pluck(:point_y, :point_x)
   end
 
   def self.rape
-    Crime.where(ucr_general: 200).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 200).pluck(:point_y, :point_x)
   end
 
   def self.robbery
-    Crime.where(ucr_general: 300).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 300).pluck(:point_y, :point_x)
   end
 
   def self.assault
-    Crime.where(ucr_general: 400).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 400).pluck(:point_y, :point_x)
   end
 
   def self.burglary
-    Crime.where(ucr_general: 500).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 500).pluck(:point_y, :point_x)
   end
 
   def self.theft
-    Crime.where(ucr_general: 600).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 600).pluck(:point_y, :point_x)
   end
 
   def self.gta
-    Crime.where(ucr_general: 700).pluck(:point_y, :point_x, :ucr_general)
+    Crime.where(ucr_general: 700).pluck(:point_y, :point_x)
   end
 end
 
